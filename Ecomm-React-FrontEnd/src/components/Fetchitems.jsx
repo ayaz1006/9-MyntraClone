23b73fab407cb96321f1906b93cdfe3122bd7e10
ayaz1006 <ayazali.9910@gmail.com>
2024-06-01 16:18:47 +0530

    Port-change

--- Ecomm-React-FrontEnd/src/components/Fetchitems.jsx
+++ Ecomm-React-FrontEnd/src/components/Fetchitems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("http://localhost:8080/items", { signal })
+    fetch("http://localhost:8800/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
